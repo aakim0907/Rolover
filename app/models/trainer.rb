98258e58b543e4ebf819af5ed536e8f2bd46c88b
Young Eun Kim <aakim0907@gmail.com>
2017-07-19 17:11:44 -0700

    Create trainer model

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -3,7 +3,16 @@ class Trainer < ApplicationRecord
   validates :training_type, presence: true, inclusion: { in: %w[obedience behavior advanced] }
 
   # add associations
+  has_many :images
   # has_many :reviews
+
+  def profile_image
+    images.select{ |img| img.type == "profile" }.first
+  end
+
+  def main_images
+    images.reject { |img| img.type == "profile" }
+  end
 
   # def rating
   #   reviewTotal = 0
