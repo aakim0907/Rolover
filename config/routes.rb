aa2c1a8620a3129deab38ca28a53087002ec5d00
Young Eun Kim <aakim0907@gmail.com>
2017-07-19 17:28:41 -0700

    Create trainer index & show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
+    resources :trainers, only: [:index, :show]
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy]
   end
