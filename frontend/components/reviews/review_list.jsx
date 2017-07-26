595f90ac9d585d8e825e0177d50c1a0af846ac57
Young Eun Kim <aakim0907@gmail.com>
2017-07-25 21:02:57 -0700

    Update trainer detail styling

--- frontend/components/reviews/review_list.jsx
+++ frontend/components/reviews/review_list.jsx
@@ -26,6 +26,8 @@ class ReviewList extends React.Component {
           { reviews.reverse().map(review => (
             <ReviewListItem key={review.id} review={review} />
           ))}
+
+          <div className='review-list-item'><br/></div>
         </div>
       );
     }
